601984fc2a267fe4d10a45d34573cc45dda7f31c
parsia_mardanpour <141585915+parsiast@users.noreply.github.com>
2024-09-10 14:31:31 +0330

    Update models.py

--- smsapp/models.py
+++ smsapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class SmsProvider (models.Model):
     TYPE_CHOICE=[('token', 'Token'),
         ('string', 'String'),]
-    apikey=models.CharField(max_length=100,default='')
+    apikey=models.CharField(max_length=100,default='{input your api key from kavenegar website}')
     ctype=models.CharField(max_length=10,choices=TYPE_CHOICE, default='token')
     def __str__(self):
         return f"{self.apikey} - {self.ctype}"
@@ -11,7 +11,7 @@ class SmsProvider (models.Model):
 class Smstosend (SmsProvider):
     created=models.DateTimeField(auto_now_add=True)
     message=models.CharField(max_length=200,blank=True,default='a blanked message')
-    receptor=models.CharField(max_length=100, blank=False, default='01')
+    receptor=models.CharField(max_length=100, blank=False, default='{input your own phone number}')
     def __str__(self):
         f"API Key: {self.apikey}, Message: {self.message}, Receptor: {self.receptor}, Created: {self.created}"
 
